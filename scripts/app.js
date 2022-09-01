b20b57a7eb425e22632f1e187e05025642bc35e5
David Castillo Fuentes <dcf82@hotmail.com>
2022-09-01 15:26:30 -0500

    Initial repository for Custom Tabs Demo

---
+++ scripts/app.js
@@ -0,0 +1,176 @@
+(function() {
+  'use strict';
+
+  const img = document.getElementById('image');
+  const configs = [
+    { button: 'butBulbasaur', image: 'images/bulbasaur.png', alt: 'Bulbasaur' },
+    { button: 'butCharmander', image: 'images/charmander.png', alt: 'Charmander' },
+    { button: 'butSquirtle', image: 'images/squirtle.png', alt: 'Squirtle' }
+  ];
+
+  configs.forEach((config) => {
+    const button = document.getElementById(config.button);
+    button.addEventListener('click', function() {
+      configs.forEach(otherConfig => {
+        if (config === otherConfig) return;
+        const otherButton = document.getElementById(otherConfig.button);
+        otherButton.classList.remove("mdl-button--colored");
+        otherButton.classList.add("mdl-button--accent");
+      });
+      img.src = config.image;
+      img.alt = config.alt;
+      button.classList.remove("mdl-button--accent");
+      button.classList.add("mdl-button--colored");
+    });
+  });
+
+  function updateNotificationStatus() {
+    const field = document.getElementById('notificationsEnabled');
+    if (!("Notification" in window)) {
+      field.textContent = "Notification doesn't exist in window";
+      return;
+    }
+    field.textContent = Notification.permission;
+    setTimeout(updateNotificationStatus, 1000);
+  }
+
+  updateNotificationStatus();
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('./service-worker.js')
+      .then(function(registration) {
+        if (!("Notification" in window)) return;
+
+        document.getElementById('butNotify')
+            .addEventListener('click', function() {
+          const title = 'Your starter';
+          const options = {
+            body: 'Are you happy with your choice?',
+            icon: img.src,
+            data: {
+              img: img.src,
+              url: window.location.origin + window.location.pathname + "?notification=true"
+            },
+            launchUrl: 'https://peconn.github.io/starters/?launched=true'
+          };
+
+          if (Notification.permission !== "granted") {
+            Notification.requestPermission();
+          }
+
+          registration.showNotification(title, options);
+        });
+
+        document.getElementById('butRequest')
+            .addEventListener('click', function() {
+          Notification.requestPermission();
+        });
+
+        document.getElementById('butCancel')
+            .addEventListener('click', function() {
+          registration.getNotifications().then(function(notifications) {
+            notifications.forEach(notification => notification.close());
+          });
+        });
+      }
+      )
+      .then(function() { console.log('Service Worker Registered'); });
+  }
+
+  if (window.Worker) {
+    const output = document.getElementById('response');
+    function appendOutput(msg) {
+      var log = output.textContent;
+      output.textContent = log + msg + "\n";
+    }
+    const button = document.getElementById('butMessageWorker');
+    button.disabled = false;
+
+    var worker = null;
+
+    button.addEventListener('click', function() {
+      if (worker == null) {
+        worker = new Worker('./math-worker.js');
+        worker.onmessage = (event) => appendOutput(event.data);
+      }
+      worker.postMessage([3]);
+    });
+
+
+    const clientButton = document.getElementById('butMessageClient');
+    clientButton.disabled = true;
+    window.addEventListener("message", function(event) {
+      console.log("[PostMessage] Got initial message.");
+      appendOutput("Got initial message.");
+
+      var port = event.ports[0];
+      if (typeof port === 'undefined') return;
+
+      console.log("[PostMessage] Got message port.");
+      appendOutput("Got message port.");
+
+      port.postMessage("Connected");
+
+      clientButton.disabled = false;
+      clientButton.addEventListener('click', function() {
+        port.postMessage(img.alt);
+      });
+
+      port.onmessage = function(event) {
+        console.log("[PostMessage] Got Message: " + event.data);
+        appendOutput(event.data);
+        port.postMessage("ACK " + event.data);
+      };
+    });
+  }
+  console.log("[PostMessage] Listener registered.");
+
+  const shareButton = document.getElementById('butShare');
+  shareButton.disabled = true;
+  if (navigator.share) {
+    shareButton.disabled = false;
+    shareButton.addEventListener('click', function() {
+      navigator.share({
+        title: 'Your Choice',
+        text: 'You picked ' + img.alt,
+        url: window.location.href
+      }).then(() => console.log("[Share] Success."))
+        .catch((error) => console.log("[Share] Error.", error));
+    });
+  }
+
+  document.getElementById('url').textContent = window.location.href;
+
+  function getCookie(name) {
+    var value = null;
+    document.cookie.split(';').forEach(cookie => {
+      cookie = cookie.trim();
+      if (cookie.startsWith(name)) {
+        value = cookie.substring(name.length + 1);
+      }
+    });
+
+    return value;
+  }
+
+  function setCookie(name, value) {
+    var year = 60*60*25*365;
+    // Yes this is ugly...
+    var domain = document.location.hostname;
+    if (domain === "localhost") {
+      document.cookie = `${name}=${value} ;max-age=${year} ;path=/`;
+    } else {
+      document.cookie = `${name}=${value} ;max-age=${year} ;domain=${domain} ;path=/`;
+    }
+  }
+
+  var visitCount = Number(getCookie("visitCount"));
+  if (isNaN(visitCount)) visitCount = 1;
+
+  document.getElementById('cookie').textContent = visitCount;
+
+  setCookie("visitCount", visitCount + 1);
+
+})();
+
---
+++ service-worker.js
@@ -0,0 +1,109 @@
+var cacheName = 'starterPWA-v2';
+var dir = '/customtabs';
+var files = [
+  '',
+  'favicon.ico',
+  'index.html',
+  'scripts/app.js',
+  'scripts/material.min.js',
+  'styles/inline.css',
+  'styles/material.light_blue-blue.min.css',
+  'images/bulbasaur.png',
+  'images/charmander.png',
+  'images/squirtle.png',
+  'math-worker.js',
+]
+
+
+  // <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.light_blue-blue.min.css" />
+  // <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching')
+      return cache.addAll(files);
+    })
+  );
+  console.log('[ServiceWorker] Cached');
+});
+
+self.addEventListener('fetch', async function(event) {
+  console.log('[ServiceWorker] Fetching ', event.request.url);
+
+  event.respondWith(async function() {
+
+    const cachePromise = caches.open(cacheName);
+    const responsePromise = fetch(event.request);
+
+    const cache = await cachePromise;
+    try {
+      const response = await responsePromise;
+      console.log("[ServiceWorker] Fetch successful.");
+      cache.put(event.request, response.clone());
+      return response;
+    } catch (error) {
+      console.log("[ServiceWorker] Fetch failed, returning cached response.");
+      // Could not connect to the internet.
+      // Return cached response.
+      const cached = await cache.match(event.request);
+      if (cached == undefined) {
+        // What can we do - 404!
+        console.log("Not in cache :-(");
+        return Promise.reject("Not cached.");
+      }
+      console.log("In cache!");
+      return cached;
+      // console.log(error);
+    }
+  }());
+});
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification clicked.');
+  event.notification.close();
+
+  const target =
+    new URL(event.notification.data.url);
+
+  event.waitUntil(async function() {
+    const clientWindows = await clients.matchAll({type: 'window'});
+
+    // Add some delays depending on the pokemon chosen.
+    const choice = event.notification.data.img;
+    if (choice.includes("charmander")) {
+      return;
+    } else if (choice.includes("squirtle")) {
+      await sleep(6000);
+    }
+
+    for (const client of clientWindows) {
+      const current = new URL(client.url);
+
+      if (!client.focus) continue;
+
+      if (target.pathname !== current.pathname) continue;
+
+      console.log('[Service Worker] Focusing.');
+      // Does getting the new client matter?
+      const newClient = await client.focus();
+
+      if (target.toString() !== current.toString()) {
+        console.log('[Service Worker] Navigating.');
+        await newClient.navigate(target.toString());
+      }
+
+      return;
+    }
+
+    if (clients.openWindow) {
+      console.log("[Service Worker] Opening Window.");
+      await clients.openWindow(target.toString());
+    }
+  }());
+});
